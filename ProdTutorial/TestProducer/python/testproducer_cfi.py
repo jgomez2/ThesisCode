7959366eb8f02071d2d4634e0055a9dff94fbfba
Jaime <jaime.arturo.gomez@cern.ch>
2014-06-21 16:20:44 -0400

    blah

--- ProdTutorial/TestProducer/python/testproducer_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-jaimeTracks = cms.EDProducer('TestProducer',
-                             trackSrc = cms.InputTag("hiGoodMergedTracks"),
-                             vertexSrc = cms.InputTag("hiSelectedVertex"),
-                             tpEffSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
-                             tpFakSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
-                             associatorMap = cms.InputTag('tpRecoAssochiGoodMergedTracks')
-                             )
