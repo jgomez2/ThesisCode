6b891e858873bb0753642873aec32c1ce81d846a
Jaime Arturo Gomez <jaime.arturo.gomez@cern.ch>
2014-06-27 12:48:42 -0500

    Update testproducer_cfi.py

--- ProdTutorial/TestProducer/python/testproducer_cfi.py
+++ ProdTutorial/TestProducer/python/testproducer_cfi.py
@@ -1,15 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 jaimeTracks = cms.EDProducer('TestProducer',
-                      trackSrc = cms.InputTag("hiGoodMergedTracks"),
+                      trackSrc = cms.InputTag("hiLowPtPixelTracks"),
                       vertexSrc = cms.InputTag("hiSelectedVertex"),
                       tpEffSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
                       tpFakSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
-                      associatorMap = cms.InputTag('tpRecoAssochiGoodMergedTracks'),
+                      associatorMap = cms.InputTag('tpRecoAssochiLowPtPixelTracks'),
                       qualityString = cms.string("highPurity"),
                       dxyErrMax = cms.double(3.0),
                       dzErrMax = cms.double(10.0),
                       ptErrMax = cms.double(0.1),
                       vertexZMax = cms.double(10.),
-                      chi2Max = cms.double(40.)
+                      chi2Max = cms.double(36.)
                       )
