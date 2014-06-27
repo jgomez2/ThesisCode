7ea7ae0f0256aed2ef4bddd6d66d9fae654a9873
Jaime Arturo Gomez <jaime.arturo.gomez@cern.ch>
2014-06-27 12:48:08 -0500

    Update efficiencyanalyzer_cfi.py

--- ProdTutorial/TestProducer/python/efficiencyanalyzer_cfi.py
+++ ProdTutorial/TestProducer/python/efficiencyanalyzer_cfi.py
@@ -1,11 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 effAna = cms.EDAnalyzer('EfficiencyAnalyzer',
-                      trackSrc = cms.InputTag("hiGoodMergedTracks"),
+                      trackSrc = cms.InputTag("hiLowPtPixelTracks"),
                       vertexSrc = cms.InputTag("hiSelectedVertex"),
                       tpEffSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
                       tpFakSrc = cms.InputTag('mergedtruth','MergedTrackTruth'),
-                      associatorMap = cms.InputTag('tpRecoAssochiGoodMergedTracks'),
+                      associatorMap = cms.InputTag('tpRecoAssochiLowPtPixelTracks'),
                       ptBins = cms.vdouble(
                       0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45,
                       0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95,
